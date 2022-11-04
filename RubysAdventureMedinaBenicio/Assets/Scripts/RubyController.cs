58c6df784907165bf197f75c551de13ad004f03f
ProdNxmbs <49ersownrams@gmail.com>
2022-11-04 09:52:50 -0700

    Adding An Enviroment

--- RubysAdventureMedinaBenicio/Assets/Scripts/RubyController.cs
+++ RubysAdventureMedinaBenicio/Assets/Scripts/RubyController.cs
@@ -14,11 +14,18 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Create 2 variables to use the unity built in axes
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        //Create our movement vector
         Vector2 position = transform.position;
+
+        //create horizontal and vertical movement
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        //setting the new position
         transform.position = position; 
     }
 }
